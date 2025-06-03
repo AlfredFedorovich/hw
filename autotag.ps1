a40896409adeb7f7c3b578155e60a1331fb279f9
AlfredFedorovich <alfkriv@gmail.com>
2025-06-03 13:52:12 +0300

    add script
    script for autotag

---
+++ autotag.ps1
@@ -0,0 +1,21 @@
+﻿$url = "git@github.com:AlfredFedorovich/hw.git"
+git clone -q $url
+Set-Location C:\Users\alfkr\hw
+$desc = git describe --tags
+
+if ($desc.Contains("-")) {
+    
+    $tag=$desc.Remove($desc.IndexOf("-"))                 #забираем таг без -
+    $split=$tag.Split(".")                                #разбиваем строку на массив
+    $patch=[string]([int]$split[-1]+1)                    #добавляем 1 к патчу
+    $newtag=$tag.Remove($tag.LastIndexOf(".")+1)+$patch   #создаем новый таг замещая в старом соледнее число
+    $newtag
+    git tag -a $newtag -m "New version"
+    git push origin $newtag  ; git push --tags
+
+           
+} else {       
+    Write-Host("No Changes")     
+}
+cd ..
+Remove-Item C:\Users\alfkr\hw -Force -Recurse
